bced534d899262fde67d7434b1776b8dc5b89af6
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-18 23:25:57 +0600

    json stringify & parse

--- script.js
+++ script.js
@@ -3,4 +3,25 @@ document.getElementById("heading").addEventListener("click",()=>{
     let head = document.getElementById("head");
     head.style.color = "green";
 })
-alert("hey everyboday keep aleart")
+
+let jsonData = {
+  "name": "Asik",
+  "age": 25,
+  "skills": ["JS", "HTML", "CSS"]
+};
+const personJSON = JSON.stringify(jsonData)
+console.log(personJSON);
+const personObj = JSON.parse(personJSON);
+console.log(personObj);
+let json2 =[
+{
+"id": 1,
+"name": "Leanne Graham",
+"username": "Bret",
+"email": "Sincere@april.biz",
+"address": {},
+"phone": "1-770-736-8031 x56442",
+"website": "hildegard.org",
+"company": {}
+},]
+console.log(json2,typeof json2);
