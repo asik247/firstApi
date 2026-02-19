de1fa1b1aa6891c4af0bdfb782b37a0d81a067e0
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-19 06:11:35 +0600

    api fetch data load and show display done

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-
+let btnLoad = document.getElementById("btnLoad");
+btnLoad.addEventListener("click", () => {
+  // console.log("btnLoad clicked");
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => {
+      // console.log(data);
+      for (d of data) {
+        // console.log(d);
+        let card = document.getElementById("card");
+        // card.innerHTML = '';
+        let div = document.createElement("div")
+        div.innerHTML = `
+          
+          <div id="child">
+          <h2>${d.name}</h2>
+          <h3>${d.id}</h3>
+          <p>${d.email}</p>
+         </div>
+        
+        `
+        card.appendChild(div)
+      }
+    })
+})
 
 
